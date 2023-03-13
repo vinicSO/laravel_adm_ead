eeb475fd85f1193cac60d7df42513c65b9b65215
Vinícius Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-03-13 16:31:31 -0300

    user repository implemented

--- app/Repositories/Eloquent/UserRepository.php
+++ app/Repositories/Eloquent/UserRepository.php
@@ -4,7 +4,7 @@ namespace App\Repositories\Eloquet;
 
 use App\Models\User as Model;
 
-class UserRepository 
+class UserRepository implements UserRepositoryInterface
 {
 
     private $model;
@@ -13,5 +13,51 @@ class UserRepository
     {
         $this->model = $model;
     }
+
+    public function getAll ( string $filter = '' ): array
+    {
+        $users = $this->model
+            ->where( function ($query) use ($filter) {
+                if ( $filter )
+                {
+                    $query->where('email', $filter);
+                    $query->orWhere('name', 'LIKE', "%{$filter}%");
+                }
+            })
+            ->get();
+
+        return $users->toArray();
+    }
+
+    public function findById ( string $id ): object | null
+    {
+        return $this->model->find($id);
+    }
+
+    public function create ( array $data ): object
+    {
+        return $this->model->create($data);
+    }
+
+    public function update ( string $id, array $data ): object | null
+    {
+        $user = $this->findById($id);
+        
+        if ( !$user ) return null;
+
+        $user->update($data);
+
+        return $user;
+    }
+
+    public function delete ( string $id ): bool
+    {
+        $user = $this->findById($id);
+
+        if ( !$user ) return null;
+
+        return $user->delete();
+    }
+
     
 }
--- app/Repositories/UserRepositoryInterface.php
+++ app/Repositories/UserRepositoryInterface.php
@@ -5,10 +5,10 @@ namespace App\Repositories\Eloquet;
 interface UserRepositoryInterface
 {
     
-    public function getAll (): array;
-    public function findById ( string $id ): object;
+    public function getAll ( string $filter = '' ): array;
+    public function findById ( string $id ): object | null;
     public function create ( array $data ): object;
-    public function update ( string $id, array $data ): object;
+    public function update ( string $id, array $data ): object | null;
     public function delete ( string $id ): bool;
     
 }
