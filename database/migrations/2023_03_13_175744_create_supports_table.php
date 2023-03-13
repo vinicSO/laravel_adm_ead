4fe90a902c1142a71500bd06af8a25e1aa149b9c
Vinícius Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-03-13 15:07:02 -0300

    create model and migration to supports and replies

---
+++ database/migrations/2023_03_13_175744_create_supports_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supports', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('lesson_id');
+            $table->uuid('user_id')->index();
+            $table->enum('status', [
+                'P', // Pendente
+                'A', // Aguardando resposta do aluno
+                'C'  // Concluído
+            ])->default('P');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('lesson_id')->references('id')->on('lessons');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supports');
+    }
+};
