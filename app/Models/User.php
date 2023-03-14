3f005a57d9ac9b4bb94089076290cf8b1ef5a57d
Vinícius Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-03-14 08:56:36 -0300

    created_at user accessor attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
@@ -41,4 +44,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected function createdAt (): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::make($value)->format('d/m/Y'),
+        );
+    }
+
+    // public function getCreatedAtAttribute ()
+    // {
+    //     return Carbon::make($this->attributes['created_at'])->format('d/m/Y');
+    // }
 }
